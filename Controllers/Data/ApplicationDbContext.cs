7203829bfa77382fa18f96d85b7474e4151d6499
CEREN ÇELİK <ceren06pasa@gmail.com>
2023-11-26 10:34:17 -0500

    Databases

--- Controllers/Data/ApplicationDbContext.cs
+++ Controllers/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShopApp.Models;
 
 namespace ShopApp.Controllers.Data
 {
@@ -7,6 +8,19 @@ namespace ShopApp.Controllers.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+
+                new Category { Id = 1, Name = "Laptops and Desktops", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Monitors", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Printers and Scanners", DisplayOrder = 3 }
+
+                );
         }
     }
 }
